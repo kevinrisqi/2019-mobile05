a0f58d3f369bfa6bd8410910444d4baf2cbb53d5
Kevin <kevinrisqi22@gmail.com>
2019-09-26 16:11:52 +0700

    Fixing in redfragment

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import polinema.ac.id.starterchapter05.R;
 import polinema.ac.id.starterchapter05.fragments.BlueFragment;
