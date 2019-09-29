77d84bfaf34feb354f6bf5ed05994ab5e3d88836
Kevin Risqi <kevinrisqi22@gmail.com>
2019-09-30 00:53:32 +0700

    Add HandstandFragment

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/PraktikumActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/PraktikumActivity.java
@@ -1,10 +1,13 @@
 package polinema.ac.id.starterchapter05.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 import polinema.ac.id.starterchapter05.R;
+import polinema.ac.id.starterchapter05.fragments.PushupsFragment;
 
 public class PraktikumActivity extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class PraktikumActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_praktikum);
+    }
+
+    public void handleClickLoadPushupsFragment(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.replace(R.id.dynamic_fragment,new PushupsFragment(),"PUSHUPS_FRAGMENT");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
